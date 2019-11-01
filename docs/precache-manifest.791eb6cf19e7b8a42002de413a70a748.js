9aebf1ef2c4ac1196e1eb7155c55cfd13c67a75b
RPSdaVllain <davllain143@gmail.com>
2019-11-01 09:32:57 +0530

    serve

---
+++ docs/precache-manifest.791eb6cf19e7b8a42002de413a70a748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95555a07916acf381ed1",
+    "url": "/static/css/main.8e9b4798.chunk.css"
+  },
+  {
+    "revision": "95555a07916acf381ed1",
+    "url": "/static/js/main.95555a07.chunk.js"
+  },
+  {
+    "revision": "7901e43b3d73830be5ad",
+    "url": "/static/js/1.7901e43b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b801bedf0cc19d41e784db877a8ccfb0",
+    "url": "/static/media/logo.b801bedf.svg"
+  },
+  {
+    "revision": "863f1c583860897c1900b80469d5d0e5",
+    "url": "/static/media/bg.863f1c58.png"
+  },
+  {
+    "revision": "e4933d1d33c954167288a238b0cdd8a5",
+    "url": "/index.html"
+  }
+];
